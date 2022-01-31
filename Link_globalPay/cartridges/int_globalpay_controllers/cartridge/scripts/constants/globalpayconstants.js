0bcdc444b5acfb42f8f63396e2f59cf7a845d5a8
Preetha R <m1080854@mindtree.com>
2022-01-31 10:31:07 +0530

    Initial commit changes for int_globalpay_controllers

---
+++ Link_globalPay/cartridges/int_globalpay_controllers/cartridge/scripts/constants/globalpayconstants.js
@@ -0,0 +1,67 @@
+'use strict';
+function globalpayConstants() {}
+var authenticationData = new Object();
+authenticationData.account_name = 'transaction_processing';
+authenticationData.channel = 'CNP';
+authenticationData.source = 'BROWSER';
+globalpayConstants.authenticationData = authenticationData;
+
+
+var authorizationData = new Object();
+authorizationData.account_name = 'transaction_processing';
+authorizationData.channel = 'CNP';
+authorizationData.source = 'BROWSER';
+authorizationData.type = 'SALE';
+authorizationData.entrymode = 'ECOM';
+authorizationData.reference = '93459c79-f3f9-427d-84d9-ca0584bb55bf';
+authorizationData.usage_mode = 'MULTIPLE';
+globalpayConstants.authorizationData = authorizationData;
+
+var paypalData = new Object();
+paypalData.account_name = 'transaction_processing';
+paypalData.channel = 'CNP';
+paypalData.source = 'BROWSER';
+paypalData.type = 'SALE';
+paypalData.paypal = 'paypal';
+paypalData.entryMode = 'paypal';
+paypalData.paymentTypeCode = 'GP_DW_PAYPAL';
+globalpayConstants.paypalData = paypalData;
+
+var googlePay = new Object();
+googlePay.account_name = 'transaction_processing';
+googlePay.channel = 'CNP';
+googlePay.type = 'SALE';
+googlePay.entryMode = 'ECOM';
+googlePay.provider = 'PAY_BY_GOOGLE';
+googlePay.paymentTypeCode = 'GP_DW_GOOGLE_PAY';
+globalpayConstants.googlePay = googlePay;
+
+var creditCardPay = new Object();
+creditCardPay.securityCode = '121';
+creditCardPay.creditCardNumber = 'creditCardNumber';
+creditCardPay.entry_mode = 'ECOM';
+creditCardPay.paymentMethod = 'CREDIT_CARD';
+globalpayConstants.creditCardPay = creditCardPay;
+
+var captureTransaction = new Object();
+captureTransaction.capture_sequence = 'FIRST';
+captureTransaction.entry_mode = 'ECOM';
+globalpayConstants.captureTransaction = captureTransaction;
+
+var applePay = new Object();
+applePay.account_name = 'transaction_processing';
+applePay.channel = 'CNP';
+applePay.type = 'SALE';
+applePay.entryMode = 'ECOM';
+applePay.provider = 'APPLEPAY';
+
+globalpayConstants.applePay = applePay;
+
+globalpayConstants.SG_CONTROLLER = 'app_storefront_controllers';
+globalpayConstants.GP_CONTROLLER = 'int_globalpay_controllers';
+globalpayConstants.GUARD = globalpayConstants.SG_CONTROLLER+'/cartridge/scripts/guard';
+globalpayConstants.APP = globalpayConstants.SG_CONTROLLER+'/cartridge/scripts/app';
+globalpayConstants.GPAPP=globalpayConstants.GP_CONTROLLER+'/cartridge/scripts/gpapp';
+globalpayConstants.SGPAGEMETA = globalpayConstants.SG_CONTROLLER+'/cartridge/scripts/meta';
+globalpayConstants.SGOBJECT = globalpayConstants.SG_CONTROLLER+'/cartridge/scripts/object';
+module.exports = globalpayConstants;
---
+++ Link_globalPay/cartridges/int_globalpay_controllers/cartridge/scripts/payment/processor/PAYPAL_EXPRESS.js
@@ -0,0 +1,99 @@
+'use strict';
+
+/* API Includes */
+var Cart = require('*/cartridge/scripts/models/CartModel');
+var PaymentMgr = require('dw/order/PaymentMgr');
+var Transaction = require('dw/system/Transaction');
+var globalpayconstants = require('*/cartridge/scripts/constants/globalpayconstants');
+var Resource = require('dw/web/Resource');
+/**
+ * This is where additional PayPal integration would go. The current implementation simply creates a PaymentInstrument and
+ * returns 'success'.
+ */
+function Handle(args) {
+    var cart = Cart.get(args.Basket);
+
+    Transaction.wrap(function () {
+        cart.removeExistingPaymentInstruments(globalpayconstants.paypalData.paymentTypeCode);
+        cart.createPaymentInstrument(globalpayconstants.paypalData.paymentTypeCode, cart.getNonGiftCertificateAmount());
+    });
+
+    return {success: true};
+}
+
+/**
+ * Authorizes a payment using a credit card. The payment is authorized by using the PAYPAL_EXPRESS processor only
+ * and setting the order no as the transaction ID. Customizations may use other processors and custom logic to
+ * authorize credit card payment.
+ */
+ function Authorize(args) {
+    var orderNo = args.OrderNo;
+    var paymentInstrument = args.PaymentInstrument;
+    var paymentProcessor = PaymentMgr.getPaymentMethod(paymentInstrument.getPaymentMethod()).getPaymentProcessor();
+    
+    var order=args.Order;
+    var globalpayconstants = require('*/cartridge/scripts/constants/globalpayconstants');
+    var globalPayPreferences = require('*/cartridge/scripts/helpers/globalPayPreferences');
+    var globalPayHelper = require('*/cartridge/scripts/helpers/globalPayHelper');
+    var URLUtils = require('dw/web/URLUtils');
+    var preferences = globalPayPreferences.getPreferences();
+    var captureMode = preferences.captureMode;
+    var Locale = require('dw/util/Locale');
+    var paypalData = {
+      account_name: globalpayconstants.paypalData.account_name,
+      channel: globalpayconstants.paypalData.channel,
+      capture_mode: captureMode.value,
+      type: globalpayconstants.paypalData.type,
+      amount: order.totalGrossPrice.value * 100,
+      currency: order.currencyCode,
+      reference: order.orderNo,
+      country: 'US',
+     // country:Locale.getLocale(request.locale.id).country, need
+      payment_method: {
+                      // name: "Doe",
+        entry_mode: globalpayconstants.paypalData.entryMode,
+        apm: {
+          provider: globalpayconstants.paypalData.paypal
+        }
+      },
+      notifications: {
+        return_url: URLUtils.https('COPlaceOrder-PayPalReturn').toString(), // "https://zzkf-006.sandbox.us01.dx.commercecloud.salesforce.com/on/demandware.store/Sites-RefArch-Site/en_US/GPPayPal-PayPalReturn",
+        status_url: URLUtils.https('COPlaceOrder-PayPalStatus').toString(), // "https://zzkf-006.sandbox.us01.dx.commercecloud.salesforce.com/on/demandware.store/Sites-RefArch-Site/en_US/GPPayPal-PayPalStatus",
+        cancel_url: URLUtils.https('COPlaceOrder-PayPalCancel').toString() // "https://zzkf-006.sandbox.us01.dx.commercecloud.salesforce.com/on/demandware.store/Sites-RefArch-Site/en_US/GPPayPal-PayPalCancel"
+      }
+    };
+    var globalPayHelper = require('*/cartridge/scripts/helpers/globalPayHelper');
+    var paypalresp = globalPayHelper.paypal(paypalData);
+    var serverErrors = [];
+    if (!empty(paypalresp) && 'success' in paypalresp && !paypalresp.success) {
+      var error = true;
+      if ('detailedErrorDescription' in authorization) { serverErrors.push(authorization.error.detailedErrorDescription); }
+    } else {
+      try {
+        Transaction.wrap(function () {
+          paymentInstrument.custom.gp_transactionid = paypalresp.id;
+          paymentInstrument.paymentTransaction.setTransactionID(args.OrderNo);
+          paymentInstrument.paymentTransaction.setPaymentProcessor(paymentProcessor);
+        });
+      } catch (e) {
+        error = true;
+        serverErrors.push(
+                          Resource.msg('error.technical', 'checkout', null)
+                      );
+      }
+    }
+
+  
+    return {authorized: true,serverErrors: serverErrors, error: error,  paypalresp: paypalresp };
+}
+
+
+/*
+ * Module exports
+ */
+
+/*
+ * Local methods
+ */
+exports.Handle = Handle;
+exports.Authorize = Authorize;
