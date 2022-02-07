04662735f5d85713bb1e9a10a066cc803496cd41
Venkatarao Madala <m1076949@mindtree.com>
2022-02-07 12:41:57 +0530

    --Corrected the paypal email and transactionstatus.

--- Link_globalPay/cartridges/int_globalpay_sfra/cartridge/controllers/GPPayPal.js
+++ Link_globalPay/cartridges/int_globalpay_sfra/cartridge/controllers/GPPayPal.js
@@ -20,16 +20,16 @@ server.use('PayPalReturn', function (req, res, next) {
   var reqMap = req.httpParameterMap;
   var orderId = req.httpParameterMap.id.toString().split('_')[2];
   var order = OrderMgr.getOrder(orderId);
-      COHelpers.sendConfirmationEmail(order, req.locale.id);
-
      if (HookManager.hasHook('app.payment.processor.globalpay_paypal')) {
      var paymentFormResult = HookManager.callHook('app.payment.processor.globalpay_paypal',
                 'Capture',
                 order
             );
       }
-      if(!empty(paymentFormResult) && paymentFormResult.paymentStatus == 'COMPLETED')
-      gputil.orderUpdate(order);
+      if(!empty(paymentFormResult) && paymentFormResult.status == 'CAPTURED'){
+            gputil.orderUpdate(order);
+      COHelpers.sendConfirmationEmail(order, req.locale.id);
+      }
   var orderId = order.orderNo;
       res.render('checkout/globalpay/threeds', {
         orderId: orderId,
