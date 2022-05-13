a172b741d681a6df46185c1dec8768ca474ca118
Venkatarao Madala <m1076949@mindtree.com>
2022-05-13 12:38:54 +0530

    --updated the code for replacing append insted of replace.

--- Link_globalPay/cartridges/int_globalpay_sfra/cartridge/controllers/Search.js
+++ Link_globalPay/cartridges/int_globalpay_sfra/cartridge/controllers/Search.js
@@ -27,59 +27,21 @@ var pageMetaData = require('*/cartridge/scripts/middleware/pageMetaData');
  * @param {renders} - isml
  * @param {serverfunction} - get
  */
- server.replace('Show', cache.applyShortPromotionSensitiveCache, consentTracking.consent, function (req, res, next) {
-    var searchHelper = require('*/cartridge/scripts/helpers/searchHelpers');
-
-    if (req.querystring.cgid) {
-        var pageLookupResult = searchHelper.getPageDesignerCategoryPage(req.querystring.cgid);
-
-        if ((pageLookupResult.page && pageLookupResult.page.hasVisibilityRules()) || pageLookupResult.invisiblePage) {
-            // the result may be different for another user, do not cache on this level
-            // the page itself is a remote include and can still be cached
-            res.cachePeriod = 0; // eslint-disable-line no-param-reassign
-        }
-
-        if (pageLookupResult.page) {
-            res.page(pageLookupResult.page.ID, {}, pageLookupResult.aspectAttributes);
-            return next();
-        }
-    }
-
-    var template = 'search/searchResults';
-
-    var result = searchHelper.search(req, res);
-
-    if (result.searchRedirect) {
-        res.redirect(result.searchRedirect);
-        return next();
-    }
-
-    if (result.category && result.categoryTemplate) {
-        template = result.categoryTemplate;
-    } else if (result.apiProductSearch.category
+ server.append('Show', cache.applyShortPromotionSensitiveCache, consentTracking.consent, function (req, res, next) {
+    var viewData = res.getViewData();
+    var result = viewData.result;
+    if (result.apiProductSearch.category
         && result.apiProductSearch.category.template
         && result.apiProductSearch.categoryID == "apple-developer-merchantid-domain-association") {
         template = result.apiProductSearch.category.template;
         result.category = result.apiProductSearch.category;
     }
 
-    var redirectGridUrl = searchHelper.backButtonDetection(req.session.clickStream);
-    if (redirectGridUrl) {
-        res.redirect(redirectGridUrl);
-    }
-
-    res.render(template, {
-        productSearch: result.productSearch,
-        maxSlots: result.maxSlots,
-        reportingURLs: result.reportingURLs,
-        refineurl: result.refineurl,
-        category: result.category ? result.category : null,
-        canonicalUrl: result.canonicalUrl,
-        schemaData: result.schemaData,
-        apiProductSearch: result.apiProductSearch
-    });
-
-    return next();
+    viewData = {
+        result = result;
+    };
+    res.setViewData(viewData);
+    next();
 }, pageMetaData.computedPageMetaData);
 
 
