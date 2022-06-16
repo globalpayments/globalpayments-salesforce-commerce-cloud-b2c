f19a0a4580bb36a77c18182e48729387246e1a43
Venkatarao Madala <m1076949@mindtree.com>
2022-06-16 16:23:17 +0530

    --Updated the code for integration test cases and alignment changes.

--- Link_globalPay/test/integration/helpers/jsonUtils.js
+++ Link_globalPay/test/integration/helpers/jsonUtils.js
@@ -10,9 +10,9 @@ function jsonUtils() {}
  * @returns {Object} - A new object with key(s) removed
  */
 jsonUtils.deleteProperties = function (srcObj, keys) {
-  var newObj = _.cloneDeep(srcObj);
-  jsonUtils.removeProps(newObj, keys);
-  return newObj;
+    var newObj = _.cloneDeep(srcObj);
+    jsonUtils.removeProps(newObj, keys);
+    return newObj;
 };
 
 /**
@@ -22,19 +22,19 @@ jsonUtils.deleteProperties = function (srcObj, keys) {
  * @param {String[]} keys
  */
 jsonUtils.removeProps = function (obj, keys) {
-  if (obj instanceof Array && obj[0] !== null) {
-    obj.forEach(function (item) {
-      jsonUtils.removeProps(item, keys);
-    });
-  } else if (typeof obj === 'object') {
-    Object.getOwnPropertyNames(obj).forEach(function (key) {
-      if (keys.indexOf(key) !== -1) {
-        delete obj[key];  // eslint-disable-line no-param-reassign
-      } else if (obj[key] != null) {
-        jsonUtils.removeProps(obj[key], keys);
-      }
-    });
-  }
+    if (obj instanceof Array && obj[0] !== null) {
+        obj.forEach(function (item) {
+            jsonUtils.removeProps(item, keys);
+        });
+    } else if (typeof obj === 'object') {
+        Object.getOwnPropertyNames(obj).forEach(function (key) {
+            if (keys.indexOf(key) !== -1) {
+                delete obj[key];  // eslint-disable-line no-param-reassign
+            } else if (obj[key] != null) {
+                jsonUtils.removeProps(obj[key], keys);
+            }
+        });
+    }
 };
 
 /**
@@ -43,13 +43,13 @@ jsonUtils.removeProps = function (obj, keys) {
  * @param {JSON} obj
  */
 jsonUtils.toPrettyString = function (obj) {
-  var prettyString;
+    var prettyString;
 
-  if (obj) {
-    prettyString = JSON.stringify(obj, null, '\t');
-  }
+    if (obj) {
+        prettyString = JSON.stringify(obj, null, '\t');
+    }
 
-  return prettyString;
+    return prettyString;
 };
 
 module.exports = jsonUtils;
